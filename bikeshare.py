d384870450b831a0e68e80baced4ba3b4bd01629
Chinedu Ike <chinedu.ike@sahara-group.com>
2020-06-27 12:45:59 +0100

    insert: Inserted comments to the get_filters function

--- bikeshare.py
+++ bikeshare.py
@@ -9,7 +9,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze for the data
+    Asks user to specify a city, month, and day to analyze. This would determine the data displayed.
 
     Returns:
         (str) city - name of the city to analyze
